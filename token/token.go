096ca0cb78fde338d8fef3faec78be1fb0384043
Ben Allen <ben@benallen.dev>
2024-12-26 12:12:29 +0100

    Oh look, our first piece of lexed code!

--- token/token.go
+++ token/token.go
@@ -2,7 +2,6 @@ package token
 
 type TokenType string
 
-
 type Token struct {
 	Type    TokenType
 	Literal string
@@ -33,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
