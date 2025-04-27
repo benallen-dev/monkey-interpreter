e7d2c88f0156805915249df9c6186cf5378f4569
Ben Allen <ben@benallen.dev>
2025-04-27 15:21:05 +0200

    Oh snap not only function calls but closures too

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -92,9 +92,74 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalIfExpression(node, env)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Body: body, Env: env}
+	case *ast.CallExpression:
+		// Eval the function
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
 	}
 
 	return nil
+}
+
+// TODO: Refactor this mess of references into a single evalCallExpression function
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) { // You _could_ just check the return array for errors, saving dev time debugging args one by one
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	// Why create a single function that just does this if you can add three levels of function calls?
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func evalProgram(program *ast.Program, env *object.Environment) object.Object {
--- object/environment.go
+++ object/environment.go
@@ -2,16 +2,24 @@ package object
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{outer: nil, store: s}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	s := make(map[string]Object)
+	return &Environment{outer: outer, store: s}
 }
 
 type Environment struct {
 	store map[string]Object
-	// I'll bet you this will contain a reference to a parent scope
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) { // _, ok pattern
-	obj, ok := e.store[name] // why not return e.store[name]?
+	obj, ok := e.store[name] // why not return e.store[name]? OOOHH because checking for parents
+	if !ok && e.outer != nil {
+		return e.outer.Get(name)
+	}
 	return obj, ok
 }
 
